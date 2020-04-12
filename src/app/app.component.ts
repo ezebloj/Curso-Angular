6e3cf9c9a45082bdfefdf0fd7eccaa41bb73c89c
ezebloj <ezebloj@gmail.com>
2020-04-12 19:34:06 -0300

    clase2-tarea revisada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'curso-angular';
+  title: string = "curso-angular";
+  alumno: any = {
+    id: 123,
+    name: "Juan Pérez",
+    mail: "juan@gmail.com",
+    avatar: "https://api.adorable.io/avatars/285/abott@adorable.png",
+    prom_gral: 8.5,
+    carrera: "Ingeniería",
+    condicion: "Regular",
+    url: "https://github.com/ezebloj",
+    materias_en_curso: ["Cálculo I", " Física I", " Álgebra"],
+  };
+  width: number = 450;
+  height: number = 350;
+  modificarNombre() {
+    this.alumno.name = "Ezequiel";
+  }
 }
