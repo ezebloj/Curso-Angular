e2d84ed63edbf8c48fda2fa60b9fe42aea7c48eb
ezebloj <ezebloj@gmail.com>
2020-04-22 14:46:06 -0300

    versión 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,4 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title: string = "curso-angular";
-  alumno: any = {
-    id: 123,
-    name: "Juan Pérez",
-    mail: "juan@gmail.com",
-    avatar: "https://api.adorable.io/avatars/285/abott@adorable.png",
-    prom_gral: 8.5,
-    carrera: "Ingeniería",
-    condicion: "Regular",
-    url: "https://github.com/ezebloj",
-    materias_en_curso: ["Cálculo I", " Física I", " Álgebra"],
-  };
-  modificarNombre() {
-    this.alumno.name = "Ezequiel";
-  }
-}
+export class AppComponent {}
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  selector: "app-content",
+  templateUrl: "./content.component.html",
+  styleUrls: ["./content.component.css"],
 })
 export class ContentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  title: string = "curso-angular";
+  alumno: any = {
+    id: 123,
+    name: "Juan Pérez",
+    mail: "juan@gmail.com",
+    avatar: "https://api.adorable.io/avatars/285/abott@adorable.png",
+    prom_gral: 8.5,
+    carrera: "Ingeniería",
+    condicion: "Regular",
+    url: "https://github.com/ezebloj",
+    materias_en_curso: ["Cálculo I", " Física I", " Álgebra"],
+  };
+  modificarNombre() {
+    this.alumno.name = "Ezequiel";
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
