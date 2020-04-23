bd14ace20a527129ed3cfd09b9aa8eb9eab4a34a
ezebloj <ezebloj@gmail.com>
2020-04-23 20:49:29 -0300

    final 23-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ContentComponent } from './content/content.component';
-import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { ContentComponent } from "./content/content.component";
+import { FooterComponent } from "./footer/footer.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -6,23 +6,15 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./content.component.css"],
 })
 export class ContentComponent implements OnInit {
-  title: string = "curso-angular";
-  alumno: any = {
-    id: 123,
-    name: "Juan Pérez",
-    mail: "juan@gmail.com",
-    avatar: "https://api.adorable.io/avatars/285/abott@adorable.png",
-    prom_gral: 8.5,
-    carrera: "Ingeniería",
-    condicion: "Regular",
-    url: "https://github.com/ezebloj",
-    materias_en_curso: ["Cálculo I", " Física I", " Álgebra"],
-  };
-  modificarNombre() {
-    this.alumno.name = "Ezequiel";
-  }
+  nombre: string = "";
+  arregloNombres: string[] = [];
 
   constructor() {}
 
   ngOnInit() {}
+
+  enviar() {
+    this.arregloNombres.push(this.nombre);
+    this.nombre = "";
+  }
 }
