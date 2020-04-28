c5dcd9ef27d1a4917a89a1e57a3889400031eebf
ezebloj <ezebloj@gmail.com>
2020-04-28 20:33:51 -0300

    finaliza formulario y muestra películas en tarjeta

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Pelicula } from "../models/pelicula.models";
 
 @Component({
   selector: "app-content",
@@ -9,6 +10,7 @@ export class ContentComponent implements OnInit {
   nombre: string = "";
   genero: string = "";
   link: string = "";
+  arregloPeliculas: Pelicula[] = [];
 
   constructor() {}
 
@@ -18,5 +20,15 @@ export class ContentComponent implements OnInit {
     // console.log(
     //   "Nombre " + this.nombre + " Género " + this.genero + " Link " + this.link
     // );
+
+    const pelicula = new Pelicula(this.nombre, this.genero, this.link);
+    this.arregloPeliculas.push(pelicula);
+    this.resetFormulario();
+  }
+
+  resetFormulario() {
+    this.nombre = "";
+    this.genero = "";
+    this.link = "";
   }
 }
