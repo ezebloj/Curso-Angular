ac3071bd4d34e7bb9186cf14e3bfd755681c95bf
ezebloj <ezebloj@gmail.com>
2020-04-30 19:40:26 -0300

    se agregaron inputs al componente card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.css"],
 })
 export class CardComponent implements OnInit {
+  @Input() titulo: string = "";
+  @Input() subtitulo: string = "";
+  @Input() link: string = "";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
