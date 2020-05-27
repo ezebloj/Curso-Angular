6a9e40bed2ad44e0ef50666728598ffd52212b4c
ezebloj <ezebloj@gmail.com>
2020-05-27 00:51:03 -0300

    .

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -30,7 +30,7 @@ export class CardComponent implements OnInit {
     //cambia el estado marcado
     this.marcado = !this.marcado;
     //armo el objeto para pasar la info en el evento
-    this.mensajeEvento.titulo = this.titulo;
+    //this.mensajeEvento.nombre = this.titulo;
     this.mensajeEvento.marcado = this.marcado;
     //emito el evento
     this.leido.emit(this.mensajeEvento);
