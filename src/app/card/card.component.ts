01d988dc5ae6b6f980bdbf8bfed6ac95ea63a5ce
ezebloj <ezebloj@gmail.com>
2020-05-31 18:30:07 -0300

    se agregó parámetros en función marcar y quitó ínea de ngif del card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -26,11 +26,11 @@ export class CardComponent implements OnInit {
 
   ngOnInit() {}
 
-  marcar() {
+  marcar(arregloPeliculas) {
     //cambia el estado marcado
     this.marcado = !this.marcado;
     //armo el objeto para pasar la info en el evento
-    //this.mensajeEvento.nombre = this.titulo;
+    this.mensajeEvento.nombre = arregloPeliculas.nombre;
     this.mensajeEvento.marcado = this.marcado;
     //emito el evento
     this.leido.emit(this.mensajeEvento);
