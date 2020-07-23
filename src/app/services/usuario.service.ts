0fb59b2226328c5706f309cba0025c6eb0169bf8
ezebloj <ezebloj@gmail.com>
2020-07-22 23:44:49 -0300

    se incorporaron verbos put y delete

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -12,16 +12,17 @@ export class UsuarioService {
   // la librería HttpClient brinda todos los métodos para conectarme con el servidor (GET, POST, PUT, DELETE, etc.)
   constructor(private http: HttpClient) {
     // esto es solo de prueba, borrar después
-    // const nuevoUsuario = {
-    //   first_name: "Ezequiel",
-    //   last_name: "Gomez",
-    //   gender: "male",
-    //   email: "ez@eze.com",
-    // };
-    // // cuando llegue la respuesta va a mostrarlo en la consola
-    // this.setUsuario(nuevoUsuario).subscribe(console.log);
+    const nuevoUsuario = {
+      first_name: "Ezequiel",
+      last_name: "Gomez",
+      gender: "male",
+      email: "ezee4@eze.com",
+    };
+    // cuando llegue la respuesta va a mostrarlo en la consola
+    this.updateUsuario(nuevoUsuario, 7626).subscribe(console.log);
   }
 
+  //obtiene todos los usuarios
   getAllUsuarios() {
     // el header son datos que acompañan a la url
     const headers = new HttpHeaders({
@@ -31,6 +32,7 @@ export class UsuarioService {
     return this.http.get(this.url, { headers });
   }
 
+  // obtiene un usuario por ID
   getUsuarioById(id: number) {
     const headers = new HttpHeaders({
       Authorization: "Bearer rFAmoCZEBz5tESyhrZFwaZo9eakV3DTym2xT",
@@ -39,10 +41,29 @@ export class UsuarioService {
     return this.http.get(url, { headers });
   }
 
+  // guarda un usuario
   setUsuario(usuario: IUsuario) {
     const headers = new HttpHeaders({
       Authorization: "Bearer rFAmoCZEBz5tESyhrZFwaZo9eakV3DTym2xT",
     });
     return this.http.post(this.url, usuario, { headers });
+  }
+
+  // actualiza un usuario
+  updateUsuario(usuario: IUsuario, id: number) {
+    const headers = new HttpHeaders({
+      Authorization: "Bearer rFAmoCZEBz5tESyhrZFwaZo9eakV3DTym2xT",
+    });
+    const url = `https://gorest.co.in/public-api/users/${id}`;
+    return this.http.put(url, usuario, { headers });
+  }
+
+  // borra un usuario
+  deleteUsuario(id: number) {
+    const headers = new HttpHeaders({
+      Authorization: "Bearer rFAmoCZEBz5tESyhrZFwaZo9eakV3DTym2xT",
+    });
+    const url = `https://gorest.co.in/public-api/users/${id}`;
+    return this.http.delete(url, { headers });
   }
 }
